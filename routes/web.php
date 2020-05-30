1bc937beb6d6cffed6d230dee34fb3a29ea48567
Nguyễn Văn Phúc <phucnvph07307@fpt.edu.vn>
2020-05-30 18:00:45 +0700

    tách route account

--- routes/web.php
+++ routes/web.php
@@ -56,20 +56,7 @@ Route::group(['middleware' => 'auth'], function () {
 
 Route::group(['middleware' => 'auth'], function () {
 
-    Route::group(['prefix' => 'account'], function(){
-
-         Route::get('/quan-ly-tai-khoan', 'AccountController@index')->name('account.list');
-         Route::post('/edit-status', 'AccountController@editstatus')->name('account.editstatus');
-         Route::get('/create', 'AccountController@create')->name('account.create');
-         Route::post('/store', 'AccountController@store')->name('account.store');
-         Route::get('/edit/{id}', 'AccountController@edit')->name('account.edit');
-         Route::post('/update', 'AccountController@updateID')->name('account.update');
-         Route::post('/check-email-update','AccountController@checkEmailUpdate')->name('account.check_email_update');
-         Route::get('/quan-ly-quyen-truy-cap', 'AccountController@quanlyquyentruycap');
-         Route::get('/phan-quyen-tai-khoan', 'AccountController@phanquyentaikhoan');
-         Route::get('/cap-nhat-thong-tin-ca-nhan', 'AccountController@capnhatthongtincanhan');
-         Route::get('/doi-mat-khau', 'AccountController@thaydoimatkhau');
-     });
+     
 
 
      Route::group(['prefix' => 'coso'], function(){
@@ -110,7 +97,7 @@ Route::group(['middleware' => 'auth'], function () {
           Route::get('/bieu-do-hop-tac-quoc-te', 'ChartController@bdhoptacquocte');
 
      });
-     
+
      Route::group(['prefix' => 'news'], function(){
           Route::get('/danh-sach-tin-tuc', 'NewsController@danhsachtintuc');
           Route::get('/chi-tiet-tin-tuc', 'NewsController@chitiettintuc');
