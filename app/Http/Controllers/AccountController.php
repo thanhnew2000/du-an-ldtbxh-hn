b82d9a6fe507b1f21694604edae931888d23023a
Nguyễn Văn Phúc <phucnvph07307@fpt.edu.vn>
2020-06-01 16:40:01 +0700

    sua search

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -20,8 +20,12 @@ class AccountController extends Controller
 {
     
     public function index(){
-        $users = User::paginate(10);
-        return view('account.list_account',compact('users'));
+        $users = DB::table('users')
+        ->leftjoin('co_so_dao_tao', 'users.co_so_dao_tao_id', '=', 'co_so_dao_tao.id')
+        ->select('users.*', DB::raw('co_so_dao_tao.ten as ten'))
+        ->paginate(10);
+ 
+        return view('account.list_account',compact('users'),['thongbao'=>'']);
     }
 
     public function create(){
@@ -40,38 +44,28 @@ class AccountController extends Controller
     public function updateID(Request $request){
         $id = $request->id;
         $this->validate($request, [
-            "email" => 'required|email|unique:users,email,'.$id,
             "phone" => 'required|numeric|digits_between:10,12',
-            // "name"  => 'required|alpha'
             'name'  => 'required|regex:/^[\pL\s\-]+$/u|min:6|max:40',
         ],[
-            "email.required" => 'Vui lòng nhập Email',
-            "email.email" => 'Email không hợp lệ',
-            "email.unique" => 'Email đã tồn tại',
-
             "phone.required" => 'Vui lòng nhập Số điện thoại',
             "phone.digits_between" => 'Số điện thoại 10 đến 12 số',
             "phone.numeric" => 'Số điện thoại không hợp lệ',
 
             "name.required" => 'Vui lòng nhập Họ và Tên',
-            // "name.alpha" => 'Họ và Tên không hợp lệ'
             'name.regex' => 'Vui lòng nhập đúng Họ và Tên',
             'name.min' => 'Họ tên ít nhất 6 ký tự',  
             'name.max' => 'Họ tên không được vượt quá 40 ký tự'
         ]);
 
         $name = $request->name;
-        $email = $request->email;
         $phone = $request->phone;
         $user = User::find($id);
 
         $user->name = $name;
-        $user->email = $email;
         $user->phone_number = $phone;
 
         $user->save();
         return redirect()->back()->with('thongbao','Cập nhật thành công !');
-        // return redirect()->route('account.list');
 
     }
 
@@ -92,6 +86,42 @@ class AccountController extends Controller
     public function checkEmailUpdate(Request $request){
 
 
+    }
+
+    public function search(Request $request){
+        $k = $request->keyword;
+        $s = $request->status;
+        $r = $request->role;
+
+        $users = DB::table('users')
+                                   ->leftjoin('co_so_dao_tao', 'users.co_so_dao_tao_id', '=', 'co_so_dao_tao.id')
+                                   ->select('users.*', DB::raw('co_so_dao_tao.ten as ten'))
+                                    ->where([
+                                        ['status', '=', $s],
+                                        ['name', 'like', '%'.$k.'%']
+                                    ])
+                                    ->orWhere([
+                                        ['status', '=', $s],
+                                        ['phone_number', 'like', '%'.$k.'%']
+                                    ])
+                                    ->paginate(10);
+     
+        $soluong = $users->count();
+        if($soluong < 1){
+            return view('account.list_account',compact('users'),['thongbao'=>'Không tìm thấy kết quả !']);
+        }                    
+        return view('account.list_account',compact('users'),['thongbao'=>'']);
+    }
+
+    public function checkName(Request $request){
+
+        $name = $request->name;
+
+        $pattern = "/^[\pL\s\-]+$/u";
+        $kq = preg_match($pattern, $name);
+        echo $kq == 1 ? "true" : "false";
+
+    
     }
 
 
