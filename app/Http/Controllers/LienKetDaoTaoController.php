aa818a468000041fc72c1db2be9d5e55745e5ad5
xuan4tt <65335756+xuan4tt@users.noreply.github.com>
2020-06-15 16:41:53 +0700

    xuan add lien ket dao tao

---
+++ app/Http/Controllers/LienKetDaoTaoController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\LienKetDaoTaoService;
+use Illuminate\Support\Facades\DB;
+
+class LienKetDaoTaoController extends Controller
+{
+    protected $ChinhSachSinhVienService;
+
+    public function __construct(LienKetDaoTaoService $LienKetDaoTaoService)
+    {
+        $this->LienKetDaoTaoService = $LienKetDaoTaoService;
+    }
+    public function tonghoplienketdaotao()
+    {
+        $limit = 10;
+        $params = request()->all();
+        $data = $this->LienKetDaoTaoService->getTongHopLienKetDaoTao($params, $limit);
+        dd($data);
+        return view('lien-ket-dao-tao.tong-hop-lien-ket-dao-tao');
+    }
+}
