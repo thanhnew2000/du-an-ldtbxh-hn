21e14d0044842346663864eb004034d022f5baaa
tuanpep <tuanbtph08879@fpt.edu.vn>
2020-06-20 14:36:12 +0700

    xóa folder view coso

--- resources/views/co-so-dao-tao/chi_tiet_co_so.blade.php
+++ resources/views/co-so-dao-tao/chi_tiet_co_so.blade.php
@@ -69,9 +69,8 @@
                     <h5 class="co-so-info">{{ $item->dia_chi}}</h5>
                 </li>
                 <li class="row">
-                    <a href="{{ route('csdt.chi-nhanh', ['id'=>$item->id]) }}" class=" btn btn-outline-info">Xem danh
-                        sách địa điểm đào tạo</a>
-                    <a href="{{route('giay-phep.danh-sach', ['id' => $item->id])}}" class=" btn btn-outline-info">Xem danh sách giấy phép</a>
+                    <a href="{{ route('csdt.chi-nhanh', ['id'=>$item->id]) }}" class=" btn btn-outline-info">Quản lý địa điểm đào tạo</a>
+                    <a href="{{route('giay-phep.danh-sach', ['id' => $item->id])}}" class=" btn btn-outline-info">Quản lý giấy phép</a>
                     <a href="{{route('csdt.thiet-lap-nghe-cs', ['csdtid' => $item->id])}}" class=" btn btn-outline-info">Xem danh sách ngành nghề</a>
                     <a href="" class=" btn btn-outline-info">Xem danh sách nhân sự</a>
                     
