bfc4923ee018fd237b8b73b0701c69dff43b0562
cuongnc-2209 <59088513+cuongnc-2209@users.noreply.github.com>
2020-06-26 16:42:35 +0700

    Update chuc_nang_phan_quyen.php

--- database/seeds/chuc_nang_phan_quyen.php
+++ database/seeds/chuc_nang_phan_quyen.php
@@ -13,42 +13,44 @@ class chuc_nang_phan_quyen extends Seeder
     public function run()
     {
 
-        Permission::create(['name' => 'them_tai_khoan']);
-        Permission::create(['name' => 'sua_tai_khoan']);
+        // Permission::create(['name' => 'them_tai_khoan']);
+        // Permission::create(['name' => 'sua_tai_khoan']);
 
-        Permission::create(['name' => 'them_moi_co_so_dao_tao']);
-        Permission::create(['name' => 'xem_chi_tiet_co_so_dao_tao']);
-        Permission::create(['name' => 'cap_nhat_co_so_dao_tao']);
+        // Permission::create(['name' => 'them_moi_co_so_dao_tao']);
+        // Permission::create(['name' => 'xem_chi_tiet_co_so_dao_tao']);
+        // Permission::create(['name' => 'cap_nhat_co_so_dao_tao']);
 
-        Permission::create(['name' => 'them_moi_dia_diem_dao_tao']);
-        Permission::create(['name' => 'cap_nhat_dia_diem_dao_tao']);
-        Permission::create(['name' => 'xoa_dia_diem_dao_tao']);
+        // Permission::create(['name' => 'them_moi_dia_diem_dao_tao']);
+        // Permission::create(['name' => 'cap_nhat_dia_diem_dao_tao']);
+        // Permission::create(['name' => 'xoa_dia_diem_dao_tao']);
 
-        Permission::create(['name' => 'them_moi_nganh_nghe']);
-        Permission::create(['name' => 'xem_chi_tiet_nganh_nghe']);
-        Permission::create(['name' => 'cap_nhat_nganh_nghe']);
-        Permission::create(['name' => 'xoa_nganh_nghe']);
+        // Permission::create(['name' => 'them_moi_nganh_nghe']);
+        // Permission::create(['name' => 'xem_chi_tiet_nganh_nghe']);
+        // Permission::create(['name' => 'cap_nhat_nganh_nghe']);
+        // Permission::create(['name' => 'xoa_nganh_nghe']);
 
-        Permission::create(['name' => 'them_moi_quan_ly_giao_vien']);
-        Permission::create(['name' => 'cap_nhat_quan_ly_giao_vien']);
+        // Permission::create(['name' => 'them_moi_quan_ly_giao_vien']);
+        // Permission::create(['name' => 'cap_nhat_quan_ly_giao_vien']);
 
 
-        Permission::create(['name' => 'them_moi_danh_sach_doi_ngu_nha_giao']);
-        Permission::create(['name' => 'chi_tiet_danh_sach_doi_ngu_nha_giao']);
-        Permission::create(['name' => 'cap_nhat_danh_sach_doi_ngu_nha_giao']);
+        // Permission::create(['name' => 'them_moi_danh_sach_doi_ngu_nha_giao']);
+        // Permission::create(['name' => 'chi_tiet_danh_sach_doi_ngu_nha_giao']);
+        // Permission::create(['name' => 'cap_nhat_danh_sach_doi_ngu_nha_giao']);
 
-        Permission::create(['name' => 'them_moi_danh_sach_doi_ngu_quan_ly']);
-        Permission::create(['name' => 'cap_nhat_danh_sach_doi_ngu_quan_ly']);
-        Permission::create(['name' => 'xem_chi_tiet_danh_sach_doi_ngu_quan_ly']);
+        // Permission::create(['name' => 'them_moi_danh_sach_doi_ngu_quan_ly']);
+        // Permission::create(['name' => 'cap_nhat_danh_sach_doi_ngu_quan_ly']);
+        // Permission::create(['name' => 'xem_chi_tiet_danh_sach_doi_ngu_quan_ly']);
 
-        Permission::create(['name' => 'them_moi_tong_hop_thuc_hien_chinh_sach_cho_sv']);
-        Permission::create(['name' => 'cap_nhat_tong_hop_thuc_hien_chinh_sach_cho_sv']);
+        // Permission::create(['name' => 'them_moi_tong_hop_thuc_hien_chinh_sach_cho_sv']);
+        // Permission::create(['name' => 'cap_nhat_tong_hop_thuc_hien_chinh_sach_cho_sv']);
 
-        Permission::create(['name' => 'them_moi_tong_hop_ket_qua_tuyen_sinh']);
-        Permission::create(['name' => 'xem_chi_tiet_tong_hop_ket_qua_tuyen_sinh']);
-        Permission::create(['name' => 'sua_chi_tiet_tong_hop_ket_qua_tuyen_sinh']);
+        // Permission::create(['name' => 'them_moi_tong_hop_ket_qua_tuyen_sinh']);
+        // Permission::create(['name' => 'xem_chi_tiet_tong_hop_ket_qua_tuyen_sinh']);
+        // Permission::create(['name' => 'sua_chi_tiet_tong_hop_ket_qua_tuyen_sinh']);
 
-        Permission::create(['name' => 'them_moi_tong_hop_ket_qua_tot_nghiep']);
-        Permission::create(['name' => 'xem_chi_tiet_tong_hop_ket_qua_tot_nghiep']);
+        // Permission::create(['name' => 'them_moi_tong_hop_ket_qua_tot_nghiep']);
+        // Permission::create(['name' => 'xem_chi_tiet_tong_hop_ket_qua_tot_nghiep']);
+
+        // Permission::create(['name' => 'cap_nhat_chi_tiet_tong_hop_ket_qua_tot_nghiep']);
     }
 }
