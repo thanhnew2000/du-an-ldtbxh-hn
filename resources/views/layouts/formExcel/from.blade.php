e1501d98c0e9b4e7a51c36720599f5e5e94d341e
thanhnv2000 <thanhnvph07471@fpt.edu.vn>
2020-07-02 10:07:51 +0700

    tat model khi tai bieu mau quan ly can bo

--- resources/views/layouts/formExcel/from.blade.php
+++ resources/views/layouts/formExcel/from.blade.php
@@ -19,7 +19,7 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Hủy</button>
-                    <button type="submit"  onclick="closeModal('closeFileBieuMau')" class="btn btn-primary">Tải</a>
+                    <button type="submit"  onclick="closeModal()" class="btn btn-primary">Tải</a>
                 </div>
             </div>
         </div>
