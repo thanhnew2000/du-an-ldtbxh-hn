95d5912b9f7e3ad24fbb9ea358f2bbf1ef84f1cf
thanhnv2000 <thanhnvph07471@fpt.edu.vn>
2020-07-03 10:55:14 +0700

    fix xuong ca

--- resources/views/layouts/dashboard/sidebar.blade.php
+++ resources/views/layouts/dashboard/sidebar.blade.php
@@ -68,17 +68,14 @@
 						<li class="m-menu__item " aria-haspopup="true"><a href="{{route('csdt.chi-nhanh')}}"
 								class="m-menu__link "><i
 									class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i><span
-<<<<<<< HEAD
 									class="m-menu__link-text">Danh sách địa điểm đào tạo</span></a></li>
 						<li class="m-menu__item " aria-haspopup="true"><a href="{{route('view-index-dot')}}"
 							class="m-menu__link "><i
 								class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i><span
 								class="m-menu__link-text">Đợt</span></a></li>
-=======
 									class="m-menu__link-text">Danh sách địa điểm đào tạo</span></a>
 						</li>
 				@endcan
->>>>>>> bdea8fdc8fb09c91d84e87a498513c4820d3c00a
 					</ul>
 				</div>
 			</li>
